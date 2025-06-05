dca05a59f1ecce9700d31d0cf67f0c5701b121b4
jyothsnaelagandula1212 <jyothsnaelagandula1212@gmail.com>
2025-06-05 12:52:12 +0530

    add Design Api

--- src/main/java/com/sample/sample/Controller/DesignController.java
+++ src/main/java/com/sample/sample/Controller/DesignController.java
@@ -1,28 +1,46 @@
 package com.sample.sample.Controller;
 
 
+
+
 import com.sample.sample.Model.Design;
 import com.sample.sample.Service.DesignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/designs")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // Optional: Enable for frontend access
 public class DesignController {
 
     @Autowired
     private DesignService designService;
+
+    @PostMapping
+    public Design addDesign(@RequestBody Design design) {
+        return designService.addDesign(design);
+    }
+
+    @PutMapping("/{id}")
+    public Design updateDesign(@PathVariable Long id, @RequestBody Design design) {
+        return designService.updateDesign(id, design);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDesign(@PathVariable Long id) {
+        designService.deleteDesign(id);
+    }
 
     @GetMapping
     public List<Design> getAllDesigns() {
         return designService.getAllDesigns();
     }
 
-    @PostMapping
-    public Design addDesign(@RequestBody Design design) {
-        return designService.addDesign(design);
+    @GetMapping("/{id}")
+    public Optional<Design> getDesignById(@PathVariable Long id) {
+        return designService.getDesignById(id);
     }
 }
--- src/main/java/com/sample/sample/Service/DesignService.java
+++ src/main/java/com/sample/sample/Service/DesignService.java
@@ -1,11 +1,13 @@
 package com.sample.sample.Service;
 
+
 import com.sample.sample.Model.Design;
 import com.sample.sample.Repository.DesignRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DesignService {
@@ -13,11 +15,30 @@ public class DesignService {
     @Autowired
     private DesignRepo designRepo;
 
+    public Design addDesign(Design design) {
+        return designRepo.save(design);
+    }
+
+    public Design updateDesign(Long id, Design updatedDesign) {
+        Optional<Design> optionalDesign = designRepo.findById(id);
+        if (optionalDesign.isPresent()) {
+            Design design = optionalDesign.get();
+            design.setName(updatedDesign.getName());
+            return designRepo.save(design);
+        } else {
+            throw new RuntimeException("Design not found with id: " + id);
+        }
+    }
+
+    public void deleteDesign(Long id) {
+        designRepo.deleteById(id);
+    }
+
     public List<Design> getAllDesigns() {
         return designRepo.findAll();
     }
 
-    public Design addDesign(Design design) {
-        return designRepo.save(design);
+    public Optional<Design> getDesignById(Long id) {
+        return designRepo.findById(id);
     }
 }
