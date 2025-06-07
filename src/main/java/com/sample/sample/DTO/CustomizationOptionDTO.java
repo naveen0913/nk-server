cf46b720dfdaf1a979695b488b4d191ba2d14755
jyothsnaelagandula1212 <jyothsnaelagandula1212@gmail.com>
2025-06-07 10:55:52 +0530

    add Design Api

---
+++ src/main/java/com/sample/sample/DTO/CustomizationOptionDTO.java
@@ -0,0 +1,52 @@
+package com.sample.sample.DTO;
+
+
+public class CustomizationOptionDTO {
+    private String optionLabel;
+    private Double originalPrice;
+    private Double oldPrice;
+    private Double discount;
+    private boolean mostPopular;
+
+    // Getters and Setters
+
+    public String getOptionLabel() {
+        return optionLabel;
+    }
+
+    public void setOptionLabel(String optionLabel) {
+        this.optionLabel = optionLabel;
+    }
+
+    public Double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(Double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public Double getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(Double oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public boolean isMostPopular() {
+        return mostPopular;
+    }
+
+    public void setMostPopular(boolean mostPopular) {
+        this.mostPopular = mostPopular;
+    }
+}
