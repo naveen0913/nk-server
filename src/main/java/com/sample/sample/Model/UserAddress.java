08f402281df52db4463e7eb09dd0d613e1a4de42
jyothsnaelagandula1212 <jyothsnaelagandula1212@gmail.com>
2025-06-14 18:43:31 +0530

    added changed files

--- src/main/java/com/sample/sample/Model/UserAddress.java
+++ src/main/java/com/sample/sample/Model/UserAddress.java
@@ -1,16 +1,8 @@
 package com.sample.sample.Model;
 
-
-
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserAddress {
 
     @Id
@@ -24,5 +16,83 @@ public class UserAddress {
     private String state;
     private String country;
     private String pincode;
-}
 
+    public UserAddress() {
+    }
+
+    public UserAddress(Long id, String addressType, String addressLine1, String addressLine2,
+                       String city, String state, String country, String pincode) {
+        this.id = id;
+        this.addressType = addressType;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pincode = pincode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+}
